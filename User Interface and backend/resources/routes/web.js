1cc2485ca463033224a231fc8405f1fdb9daf790
Nikesh Kazi Lepcha <nikeshepcha7gmailcom>
2021-02-19 22:45:03 -0800

    set up the backend functions for each req from the frontend

--- User Interface and backend/resources/routes/web.js
+++ User Interface and backend/resources/routes/web.js
@@ -17,5 +17,50 @@ module.exports = function(app) {
             });
         });  
     });
+
+    app.post('/newUser', (req, res) => {
+        let {username} = req.body;
+        new User({
+            username,
+        }).save((err, user) => {
+            req.session.userId = user._id; //temp
+            res.json({
+                id : user._id,
+            });
+        });
+    });
+
+    app.post('/topUp', (req, res) => {
+        let {topUpAmt} = req.body;
+        let userId = req.session.userId;
+        User.findOne({_id : userId}, (err, user) => {
+            if(err) {
+                //handle error
+                throw err
+            }
+            user.balance += topUpAmt;
+            user.save((err, user) => {
+                res.json({
+                    newBalance : user.balance,
+                });
+            });
+        });
+    });
+
+    app.get('/getQR', (req, res) => {
+        let userId = req.session.userId;
+        User.findOne({_id : userId}, (err, user) => {
+            if(err) {
+                //handle error
+                throw err;
+            }
+            res.json({
+                id : userId,
+                username : user.username,
+                balance : user.balance,
+            });
+        });
+    });
+
 }
 
